b76645c50b00cdc0c6e73d311ddc69c549c51972
zhiktang <zhikai.tang123@gmail.com>
2022-10-16 15:15:40 -0500

    basic ahh bot

--- bot.js
+++ bot.js
@@ -9,6 +9,20 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
 // Login to Discord with your client's token
 client.login(token);
  
