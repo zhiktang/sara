8ed489f19c75ca99944449acb23b9d83465e462b
zhiktang <zhikai.tang123@gmail.com>
2022-10-16 15:05:17 -0500

    shitty ahh tutorial

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
 
-// Create a new client instance
-const client = new Discord.Client({ intents: [ 'DIRECT_MESSAGES', 'GUILD_MESSAGES' ] });
+const { token } = require('./config.json');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
