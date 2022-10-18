99f5ee23b81122fe21979bb99777324ebbe75f69
zhiktang <zhikai.tang123@gmail.com>
2022-10-18 08:38:26 -0500

    add initial funny prompt

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@
 
 const { token } = require('./config.json');
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const {exec} = require('child_process');
+const scriptLocation = './script.sh';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 // When the client is ready, run this code (only once)
@@ -21,6 +23,19 @@ client.on('interactionCreate', async interaction => {
 	} else if (commandName === 'user') {
 		await interaction.reply('User info.');
 	}
+	//the funny part
+	else if (commandName === 'test'){
+		await interaction.reply('test');
+		prompt = interaction.options.getString('prompt');
+		exec('python ${scriptLocation}', (err, stdout, stderr) => {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			console.log(stdout);
+			// interaction.reply(stdout);
+		});	
+	} 
 });
 
 // Login to Discord with your client's token
--- deploy-commands.js
+++ deploy-commands.js
@@ -5,6 +5,7 @@ const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('test').setDescription('test').addStringOption(option => option.setName('prompt').setDescription('prompt')),
 ]
 	.map(command => command.toJSON());
 
